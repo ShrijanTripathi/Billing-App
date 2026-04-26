73bc74eb4f9257c64254ba8c425357979d970cdc
ShrijanTripathi <shrijan8960@gmail.com>
2026-04-27 01:34:02 +0530

    added backend api

--- backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const env = require("./env");
-
-async function connectDb() {
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(env.mongoUri);
-}
-
-module.exports = connectDb;
